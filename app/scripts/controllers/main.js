66a2858ac0ebecfb6644a4a9a79c2bfc5478641a
uzeltinger <fabiouz@gmail.com>
2019-05-22 12:18:35 -0300

    inicio v 4

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,94 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name werpiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the werpiApp
+ */
+angular.module('werpiApp')
+  .controller('MainCtrl', function ($scope, $rootScope, $location, $filter, api, $window, SELLER) {
+
+    $scope.parkings = [];
+    $scope.myPosition = { long: -58.3810793, lat: -34.606510 };
+    $scope.showAll = 4;
+    $scope.totalParkings = 0;
+    $scope.urlIdTeatro = null;
+    $scope.mostrarDistancia = false;
+
+    if ($location.search().id_teatro) {
+      $scope.urlIdTeatro = $location.search().id_teatro;
+      $window.sessionStorage.setItem("urlIdTeatro", $scope.urlIdTeatro);
+      $scope.mostrarDistancia = true;
+    } else {
+      if ($window.sessionStorage.getItem('urlIdTeatro')) {
+        $scope.urlIdTeatro = $window.sessionStorage.getItem('urlIdTeatro');
+      }
+    }
+
+    $scope.urlfuncion = null;
+
+    if ($location.search().funcion) {
+      $scope.urlfuncion = $location.search().funcion;
+      $window.sessionStorage.setItem("urlfuncion", $scope.urlfuncion);
+    } else {
+      if ($window.sessionStorage.getItem('urlfuncion')) {
+        $scope.urlfuncion = $window.sessionStorage.getItem('urlfuncion');
+      }
+    }
+
+    api.plateanet.getTeatros().$promise.then(function (response) {
+      $scope.allTeatros = response.data;
+      $scope.selectedTeatro = $scope.allTeatros[0];
+      $scope.allTeatros = $filter('orderBy')($scope.allTeatros, 'ComboDescription');
+      $scope.showTeatros = true;
+      var places = [];
+      angular.forEach($scope.allTeatros, function (teatro) {
+
+        if ($scope.urlIdTeatro && $scope.urlIdTeatro != '') {
+          if ($scope.urlIdTeatro == teatro.Id) {
+            var place = { id: 'teatro-' + teatro.Id, info: { icon: "images/map-plateanet.png", lat: teatro.Latitud, long: teatro.Longitud, name: teatro.Name, description: teatro.Name } };
+            places.push(place);
+            $scope.URLFixedPostion = { name: '', coords: { longitude: teatro.Longitud, latitude: teatro.Latitud } };
+          }
+        } else {
+          var place = { id: 'teatro-' + teatro.Id, info: { icon: "images/map-plateanet.png", lat: teatro.Latitud, long: teatro.Longitud, name: teatro.Name, description: teatro.Name } };
+          places.push(place);
+        }
+      });
+      $scope.otherPlaces = places;
+      $scope.myPosition.lat = places[0].info.lat;
+      $scope.myPosition.long = places[0].info.long;
+      getList();
+    }, function (response) { 
+      $scope.showTeatros = false; 
+      getList();
+    });
+
+    
+
+    function getList() {
+      api.parking.lista({ 'sellerId': SELLER.sellerID, 'latitude': $scope.myPosition.lat, 'longitude': $scope.myPosition.long }).$promise.then(function (response) {
+        var parkings = response.data.parkings;
+        if ($location.search().id_teatro) {
+          parkings=parkings.slice(0, 4);
+        }
+        $scope.totalParkings = parkings.length;
+        angular.forEach(parkings, function (info, key) {
+          var buttonReservar = '<button type="button" id="btn-reservar" onClick="window.sessionStorage.setItem(\'reservarUrl\', \'#/buy/?parkingId=' + info.parkingId + '&lat=' + $scope.myPosition.lat + '&lng=' + $scope.myPosition.long + '\');window.location.href=\'#/buy/?parkingId=' + info.parkingId + '&lat=' + $scope.myPosition.lat + '&lng=' + $scope.myPosition.long + '\'" class=""><img src="./images/reservar.svg" alt="">Reservar</button>';
+          info.buttonReservar = buttonReservar;
+        });
+        $scope.parkings = parkings;
+      });
+    }
+
+    $scope.reservar = function (parking) {
+      window.sessionStorage.setItem('reservarUrl', '#/buy/?parkingId=' + parking.parkingId + '&lat=' + $scope.myPosition.lat + '&lng=' + $scope.myPosition.long);
+      window.location.href = '#/buy/?parkingId=' + parking.parkingId + '&lat=' + $scope.myPosition.lat + '&lng=' + $scope.myPosition.long;
+    }
+
+    $scope.verMas = function () {
+      $scope.showAll = 999;
+    }
+  });
---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,75 @@
+'use strict';
+angular.module('werpiApp')
+  .controller('ProfileCtrl', function ($scope, $stateParams, Session, $rootScope, api, notify, $window) {
+
+    $scope.user = Session.getUser();
+    var token = Session.getSession().token;
+    $scope.newUserData = {};
+    $scope.newUserData.licencePlate = $scope.user.licencePlate;
+    $scope.newUserData.firstName = $scope.user.firstName;
+    $scope.newUserData.lastName = $scope.user.lastName;
+    if($scope.user.phone){
+      $scope.newUserData.phoneTypeId = $scope.user.phone.phoneType.phoneTypeId.toString();
+      $scope.newUserData.phoneNumber = $scope.user.phone.phoneNumber;
+    }
+    $scope.newUserData.addressTypeId = 1;//$scope.user.address.addressType.addressTypeId;    
+    if($scope.user.address){
+      $scope.newUserData.street = $scope.user.address.street;
+      $scope.newUserData.streetNumber = $scope.user.address.number;
+      $scope.newUserData.floor = $scope.user.address.floor;
+      $scope.newUserData.zipcode = $scope.user.address.zipCode;
+      $scope.newUserData.provinceId = $scope.user.address.province.provinceId.toString();
+      $scope.newUserData.countryId = $scope.user.address.country.countryId;
+    }else{
+      $scope.newUserData.countryId = "AR";
+    }
+    
+
+    api.province.getProvinces().$promise.then(function (result) {
+      $scope.provinces = result.data;      
+    });
+
+    $scope.phoneTypes = [
+      {"phoneTypeId":"1","phoneType":"Fijo"},
+      {"phoneTypeId":"2","phoneType":"Movil"}
+    ]
+
+    $scope.newUserData.phoneTypeId='1';
+
+    $scope.showModifProfileBanner = !($window.sessionStorage.getItem("showModifProfileBanner") == "false");
+
+    $scope.downloadWerpi = function () {
+      $window.open('http://onelink.to/qj6zn5', '_blank');
+    }
+    $scope.closeBanner = function () {
+      $scope.showModifProfileBanner = false;
+      $window.sessionStorage.setItem("showModifProfileBanner", false);
+    }
+
+    $scope.setNewUserData = function (newUserData) {
+      if ($scope.profile_form.$valid) {
+
+        if (!$scope.newUserData.provinceId){
+            notify({ messageTemplate: '<i class="fa fa-exclamation-circle big-fa"></i><div class="notify-text"> Seleccione una provincia del listado</div>', classes: 'alert-notify', position: 'right' });
+            return;
+        }
+
+        var data = { id: $scope.user.userId };
+        $scope.loading = true;
+        api.user.setNewUserData(data, newUserData).$promise.then(function (response) {
+          Session.setUser(response.data.user);    
+          $scope.loading = false;
+          notify({ messageTemplate: '<i class="fa fa-check big-fa"></i><div class="notify-text">Se han guardado los nuevos datos</div>', classes: 'alert-notify', position: 'right' });
+        }, function (response) {
+          $scope.loading = false;
+          var msg = "No se pudo actualizar la información. ";
+          if (response.data && response.data.errors && response.data.errors.error && response.data.errors.error.errorDetail) {
+            msg += response.data.errors.error.errorDetail;
+          }
+          notify({ messageTemplate: '<i class="fa fa-exclamation-circle big-fa"></i><div class="notify-text">' + msg + '</div>', classes: 'alert-notify', position: 'right' });
+        });
+      }
+    
+    }
+
+  });
