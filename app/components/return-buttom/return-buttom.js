937f97e29a6234c097e76871c83ab0422f3efb44
uzeltinger <fabiouz@gmail.com>
2019-05-30 14:20:14 -0300

    cambio evento volver

--- app/components/return-buttom/return-buttom.js
+++ app/components/return-buttom/return-buttom.js
@@ -1,4 +1,33 @@
 angular.module("returnButtom", [])
-  .component("returnButtom", {
+  .component("returnButtom", {    
+    controller: function($scope, $rootScope) {
+      this.$onInit = function() {
+        console.log(this);
+      };
+
+      $scope.goBack = function(event) {
+        event.preventDefault();
+        console.log("$rootScope.paySection", $rootScope.paySection);
+        if($rootScope.paySection === 'step-two' && !$rootScope.estacionarSection) {
+          $rootScope.paySection = 'step-one';
+          return;
+        }
+    
+        if($rootScope.goBackCallback){
+          $rootScope.goBackCallback();
+          $rootScope.goBackCallback = ''; // safety reset
+          return;
+        }
+        //$state.go('main');
+        if($rootScope.loginPage){
+          $rootScope.loginPage = false;
+          $state.go('main');
+          return;
+        }
+        window.history.back();
+      }
+
+
+    },
     templateUrl: "./components/return-buttom/return-buttom.html"
   });
